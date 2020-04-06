f0c2bd0f76eb2083b4cd53b26b129d9e6b4a9d39
Sydney Werbach <sydwer@gmail.com>
2020-04-05 20:58:06 -0600

    Added in Chart.vue component and importing d3 into App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chart />
   </div>
 </template>
 
 <script>
+import * as d3 from 'd3'
+
 import HelloWorld from './components/HelloWorld.vue'
+import Chart from './components/Chart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Chart
   }
 }
 </script>
